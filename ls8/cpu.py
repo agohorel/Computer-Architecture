ab28a5ebdfcdbe176446abfbf4b442960130b486
agohorel <3317203+agohorel@users.noreply.github.com>
2020-06-16 18:19:32 -0400

    refactor instruction handling

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,10 +11,12 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
-        self.LDI = 0b10000010
-        self.PRN = 0b01000111
-        self.HLT = 0b00000001
-        self.MUL = 0b10100010
+        self.instructions = {
+            0b10000010: self.ldi,
+            0b01000111: self.prn,
+            0b00000001: self.hlt,
+            0b10100010: self.mul
+        }
 
     def ram_read(self, pc):
         return self.ram[pc]
@@ -29,9 +31,10 @@ class CPU:
             raise NameError("No input program specified, exiting...")
 
         program_name = sys.argv[1]
+        address = 0
 
         with open(program_name) as p:
-            for address, line in enumerate(p):
+            for line in p:
                 line = line.split("#")[0]
 
                 try:
@@ -40,6 +43,7 @@ class CPU:
                     continue
 
                 self.ram[address] = instruction
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -77,18 +81,10 @@ class CPU:
 
         while running == True:
             ir = self.ram[self.pc]
+            instruction = self.instructions.get(ir)
 
-            if ir == self.LDI:
-                self.ldi()
-
-            elif ir == self.PRN:
-                self.prn()
-
-            elif ir == self.MUL:
-                self.mul()
-
-            elif ir == self.HLT:
-                running = self.hlt()
+            if instruction:
+                self.instructions[ir]()
 
     def ldi(self):
         address = self.ram[self.pc+1]
@@ -103,8 +99,7 @@ class CPU:
         self.pc += 2
 
     def hlt(self):
-        self.pc += 1
-        return False
+        sys.exit(0)
 
     def mul(self):
         address_1 = self.ram[self.pc+1]
